4e4494442cebd606da57250ab25e92b7b427024f
LucasLovizzio <llovizzio@gmail.com>
2023-09-23 12:59:40 -0300

    . 	new file:   TP2/7.cpp

---
+++ TP2/7.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+const int MAX=100;
+int dimensionLogica;
+
+void cargar(float arreglo[], int cantidad , int &dl)
+{
+    float num;
+    for (int i=0; i<cantidad; i++)
+    {
+        cout << "Número para almacenar: ";
+        cin >> num;
+        arreglo[i]=num; 
+        dl++;
+    }
+}
+
+void imprimirArreglo(float arreglo[], int dl)
+{
+    for(int j=0 ; j<dl ; j++)
+    {
+        cout << arreglo[j] << " ";
+    }
+}
+
+int main()
+{
+    float numeros[MAX];
+    int cantidad;
+    do
+    {
+        cout << "Ingrese la cantidad de numeros a almacenar: ";
+        cin >> cantidad;
+    } while (cantidad > MAX);
+    cargar(numeros, cantidad , dimensionLogica);
+    imprimirArreglo(numeros, dimensionLogica);
+}
