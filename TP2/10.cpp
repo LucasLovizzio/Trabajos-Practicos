517660a4fee97c52a1f7758d19a2f0c7d35d4037
LucasLovizzio <llovizzio@gmail.com>
2023-09-25 11:39:59 -0300

    ej10 	modified:   TP2/10.cpp

--- TP2/10.cpp
+++ TP2/10.cpp
@@ -12,3 +12,89 @@ d) Finalmente, imprimir todo el arreglo.
 using namespace std;
 
 const int MAX = 200;
+int dimlogica = 3;
+void agregarSocio(int DNI, int arreglo[], int &dim_Logica)
+{
+    arreglo[dim_Logica]=DNI; 
+    dim_Logica++;
+}
+
+void eliminarSocio(int DNI, int arreglo[], int &dim_Logica)
+{
+    int i = 0;
+    while (i<dim_Logica && arreglo[i] != DNI)
+    {
+        i++;
+    }
+    if (arreglo[i]==DNI)
+    {
+        for (int j = i; j<dim_Logica-1 ; i++)
+        {
+            arreglo[j]=arreglo[j+1];
+        }
+        dim_Logica--;
+    }
+}
+
+void buscarSocio(int DNI, int arreglo[], int dim_Logica)
+{
+    bool encontrado=false;
+    for (int i = 0 ; i<dim_Logica ; i++)
+    {
+        if (arreglo[i]==DNI)
+            encontrado = true;
+    }
+    if (encontrado==true){
+        cout << "Cuota al dia\n";}
+    else{
+        cout << "Socio con deuda\n";}
+}
+
+void menu()
+{
+    int socios[MAX]={44444444,45444444,46444444};
+    int opcion;
+    int DNI;
+    do
+    {
+        cout << "1 - Agregar socio \n";
+        cout << "2 - Eliminar socio \n";
+        cout << "3 - Buscar socio \n";
+        cout << "0 - Salir\n\n";
+        cout << "Ingrese una opcion: ";
+        cin >> opcion;
+
+        switch (opcion)
+        {
+        case 1:
+            cout << "Ingrese el DNI del nuevo socio: ";
+            cin >> DNI; cout << endl;
+            agregarSocio(DNI, socios, dimlogica);
+            break;
+        case 2:
+            cout << "Ingrese el DNI del socio a eliminar: ";
+            cin >> DNI; cout << endl;
+            eliminarSocio(DNI, socios, dimlogica);
+            break;
+        case 3:
+            cout << "Ingrese el DNI del socio: ";
+            cin >> DNI; cout << endl;
+            buscarSocio(DNI, socios, dimlogica);
+            break;
+        case 0:
+            break;
+        default:
+            cout << "ERROR (opcion incorrecta)...";
+            break;
+        }
+
+    } while (opcion!=0);
+    
+}
+
+
+int main()
+{
+    menu();
+    return 0;
+}
