a9c76cc76ed865ac3099b9ddb2be9108ca40b2f0
Lucas <llovizzio@gmail.com>
2023-10-08 20:34:31 -0300

    .

--- TP2/21.cpp
+++ TP2/21.cpp
@@ -1,8 +1,24 @@
+/*Dado el siguiente programa, indicar la salida en cada punto señalado*/
+
 #include <iostream>
 using namespace std;
+int main() {
+    int *p1, *p2;
 
-int main()
-{
-          
+    p1 = new int;
+    *p1 = 42;
+    p2 = p1;
+    cout << *p1 << endl; // 42
+    cout << *p2 << endl; // 42
+
+    *p2 = 53;
+    cout << *p1 << endl; // 53
+    cout << *p2 << endl; // 53
+
+    p1 = new int; 
+    *p1 = 88;
+    cout << *p1 << endl; // 88
+    cout << *p2 << endl; // 53
+    cout << "FIN"; //C. Luego de ejecutar esta línea
+    return 0;
 }
-
