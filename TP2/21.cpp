da8b0e897af6541e0f4b65d58a98d987b37f156a
Lucas <llovizzio@gmail.com>
2023-10-09 11:13:20 -0300

    .

---
+++ TP2/21.cpp
@@ -0,0 +1,10 @@
+/*
+Escribir un programa que almacene en un arreglo los datos de los alumnos de una escuela luego de finalizado elaño académico. 
+Como máximo, puede haber 5000 alumnos. De cada alumno se debe guardar el nombre y la
+calificación obtenida en el primer, segundo y tercer trimestre del año académico. Imprimir el listado de
+nombres de alumnos mostrando un mensaje de "APTO" si el alumno supera o iguala la calificación de 5 para
+todas las notas o "NO APTO" si no lo alcanza.
+Utilizar un arreglo como campo del struct para almacenar las tres calificaciones.
+*/
+#include <iostream>
+using namespace std;
